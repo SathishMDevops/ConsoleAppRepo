83a027bf2a834e8f3219bb95f9af553b3d683c02
SathishkumarM <user@DESKTOP-LPERURE>
2024-02-24 22:14:23 +0530

    New Changes in program file

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Vino");
             Console.WriteLine("Sathish");
             Console.ReadLine();
         }
